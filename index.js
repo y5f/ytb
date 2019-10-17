3841fdceb1839fe3a231619b368518bd254381ca
y5f <ysf.9203@gmail.com>
2019-10-17 14:34:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 const ffmpeg = require('fluent-ffmpeg');
 const ytdl = require('ytdl-core');
